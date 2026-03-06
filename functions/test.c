ff514765f21db56e6332924df346d85864b94cc4
Kit <4jrkit1@gmail.com>
2026-03-06 23:06:41 +0100

    fear : testing functions

---
+++ functions/test.c
@@ -0,0 +1,21 @@
+// testing function behavior
+
+#include<stdio.h>
+
+int sum(int n, int y)
+{
+    return n / y;
+}
+
+int main()
+{
+    int x = 12 , z = 6;
+
+    printf("%d \n", sum(z, x)); // no such thing as kargs and pargs
+
+    // only paosition matters
+
+
+
+    return 0;
+}
