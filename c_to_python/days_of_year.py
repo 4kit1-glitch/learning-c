e1c54c234514e99fed735b6e2ca8ff9134dae7f4
Kit <4jrkit1@gmail.com>
2026-03-11 19:23:52 +0100

    days of year

--- c_to_python/days_of_year.py
+++ c_to_python/days_of_year.py
@@ -1,9 +1,15 @@
-from functools import reduce
 
 day = int(input("enter the day"))
 month = int(input("enter the month"))
 year = int (input("enter the year"))
 
-# def is_leap(year):
-#     if year % 4 == 0:
-#         if
+month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_sum = 0
+
+def is_leap(year):
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return True
+    return False
+
