22e41662684702fed58dc87e969c0b6f2a75ee85
Kit <4jrkit1@gmail.com>
2026-03-11 12:29:31 +0100

    collected user info

---
+++ c_to_python/days_of_year.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+day = int(input("enter the day"))
+month = int(input("enter the month"))
+year = int (input("enter the year"))
+
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if
