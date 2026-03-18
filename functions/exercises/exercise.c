5c75525696f147647f0ee5224717482fccb055c2
Kit <4jrkit1@gmail.com>
2026-03-18 18:07:36 +0100

    small fix

--- functions/exercises/exercise.c
+++ functions/exercises/exercise.c
@@ -3,14 +3,11 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-double triangle_area(double base, double hieght)
-{
+double triangle_area(double base, double hieght){
     double product = base * hieght;
-
     return product / 2;
 }
-bool is_between(int x, int y, int n)
-{
+bool is_between(int x, int y, int n){
     if((1 < x && (n-1) > x) && 1 < y && n-1 > y){
         return true;
     }
@@ -18,9 +15,17 @@ bool is_between(int x, int y, int n)
         return false;
     }
 }
+int gdc(int num1, int num2){
+    int r;    // remainder
+    
+    if(num2 == 0){
+        return num1;
+    }
+    r = num1 % num2;
+    gdc(num2, r);
+}
 
-int main()
-{
+int main(){
 
 
 
--- incomplete/magicsq.c
+++ incomplete/magicsq.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 
 int main(){
-    int size , row, col, next_row, next_col;
-    int magic_sqr[size][size];
+    int size, row, col, next_row, next_col;
 
     printf("Enter the size of the magic square (odd integer btw 1 and 99): ");
     scanf("%d", &size);
+
+    int magic_sqr[size][size];
 
     // filling square with 0 for easy manipulation
     for(int i = 0; i < size; i++){
@@ -20,6 +21,7 @@ int main(){
     col = size / 2;
     magic_sqr[row][col] = 1;
 
+    // filling magic square
     for(int num = 2; num <= (size * size); num++){
         next_row = row - 1;
         next_col = col + 1;
@@ -32,27 +34,23 @@ int main(){
         }
 
         if(magic_sqr[next_row][next_col] != 0){
-            next_row = size + 1;
-            magic_sqr[next_row][next_col] = num;
+            printf("X,");
+            next_row = row + 1;
+            next_col = col;
         }
-        else{
-            magic_sqr[next_row][next_col] = num;
-        }
-    }
+        magic_sqr[next_row][next_col] = num;
+        
 
+        row = next_row;
+        col = next_col;
+    }
+    printf("\n");
+    // printing magic square
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            printf("%2d", magic_sqr[i][j]);
+            printf("%5d", magic_sqr[i][j]);
         }
         printf("\n");
     }
-
-
-    
-
-
-
-
-
     return 0;
 }
