824f1f489a5a154cc26eebf67f0297bdad6c0307
Kit <4jrkit1@gmail.com>
2026-03-18 20:15:26 +0100

    small exercise done

--- functions/exercises/exercise.c
+++ functions/exercises/exercise.c
@@ -61,6 +61,15 @@ int swap(int a, int b){
 
     return a;
 }
+int average(int arr[], int n){
+    int lenght = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+
+    for(int i = 0; i < n; i++){
+        sum = sum + arr[i];
+    }
+    return sum / n;
+}
 
 int main(){
     
