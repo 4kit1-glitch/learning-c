72a53073a858a8cd03cca109935ba7b27bc6b616
Kit <4jrkit1@gmail.com>
2026-03-19 14:21:47 +0100

    decode mystery func it converts a number to base 2

--- functions/exercises/mystery.c
+++ functions/exercises/mystery.c
@@ -2,16 +2,18 @@
 
 #include<stdio.h>
 
+// this function converts from base 10 to base 2
 void pb(int n){
+    int i = n;
+    printf("%d\n", i);
     if(n != 0){
         pb(n / 2);
         putchar('0' + (n % 2));
     }
 }
 int main(){
-
-
-
+    pb(25);
+    putchar('h');
 
     return 0;
 }
