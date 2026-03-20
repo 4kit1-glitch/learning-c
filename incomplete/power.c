428862e51f5a46d486697720174ee8f19279015e
Kit <4jrkit1@gmail.com>
2026-03-20 23:17:28 +0100

    feat: completed power.c

---
+++ incomplete/power.c
@@ -0,0 +1,26 @@
+// more effective algorithm for calculating power O(log n)
+
+#include<stdio.h>
+
+int power(int x, int n) {
+    if( n == 0) {
+        return 1;
+    }
+    else if (n % 2 == 0) {
+        return (power(x, n/2), 2);
+    }
+    else {
+        return x * (power(x, n - 1));
+    }
+}
+int main() {
+    int x, n, result;
+
+    printf("Enter the value (X^n): ");
+    scanf("%d,%d", &x, &n);
+
+    result = power(x, n);
+    printf("%d\n", result);
+
+    return 0;
+}
