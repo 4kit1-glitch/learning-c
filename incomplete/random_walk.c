7d98aabc692dcc59f37dc5dfc93c41a5fc7e73bf
Kit <4jrkit1@gmail.com>
2026-03-20 21:57:01 +0100

    fix: control move boundaries

--- incomplete/random_walk.c
+++ incomplete/random_walk.c
@@ -10,15 +10,15 @@
 
 #include<stdio.h>
 
-void create_walk_area(char walk_area[][AREA_SIZE]){
-    for(int i = 0; i < AREA_SIZE; i++){
-        for(int j = 0; j < AREA_SIZE; j++){
+void create_walk_area(char walk_area[][AREA_SIZE]) {
+    for (int i = 0; i < AREA_SIZE; i++) {
+        for (int j = 0; j < AREA_SIZE; j++) {
             walk_area[i][j] = '.';
         }
     }
 }
 void generate_random_walk(char walk_area[][AREA_SIZE]){
-    int move, row, col, next_row, next_col;
+    int move, row, col, next_row, next_col, count;
     char indicator = 'A'; // indicator what diplays after every move
     char next_move;
 
@@ -29,40 +29,68 @@ void generate_random_walk(char walk_area[][AREA_SIZE]){
 
     // start position
     walk_area[row][col] = indicator;
+    count = 1;
 
-    next_move = rand() % MOVE_COUNT;
+    while (count <= 26) {
+        next_move = rand() % MOVE_COUNT;
 
     // move behavior
-    if(next_move == 0){
-        next_row = row - 1;
-        next_col = col;
+        if (next_move == 0) {
+            next_row = row - 1;
+            next_col = col;
+        }
+        else if (next_move == 1) {
+            next_row = row + 1;
+            next_col = col;
+        }
+        else if (next_move == 2) {
+            next_col = col - 1;
+            next_row = row;
+        }
+        else if (next_move == 3) {
+            next_col = col + 1;
+            next_row = row;
+        }
+        else {
+            next_col = col;
+            next_row = row;
+        }
+
+        // setting move boundaries
+        bool out_of_bounds =
+            next_row < 0           ||
+            next_col < 0           ||
+            next_row >= AREA_SIZE  ||
+            next_col >= AREA_SIZE;
+        
+        if (out_of_bounds) {
+            continue;
+        }
+
+        // normal move
+        if (!out_of_bounds) {
+            row = next_row;
+            col = next_col;
+            walk_area[next_row][next_col] = indicator + count;
+            count++;
+        }
+
+        // handling move rewrites
+
     }
-    else if(next_move == 1){
-        next_row = row + 1;
-        next_col = col;
-    }
-    else if(next_move == 2){
-        next_col = col - 1;
-        next_row = row;
-    }
-    else if(next_move == 3){
-        next_col = col + 1;
-        next_row = row;
-    }
-    
     
 }
-void print_area(char walk_area[][AREA_SIZE]){
-    for (int i = 0; i < AREA_SIZE; i++){
-        for (int j = 0; j < AREA_SIZE; j++){
+void print_area(char walk_area[][AREA_SIZE]) {
+    for (int i = 0; i < AREA_SIZE; i++) {
+        for (int j = 0; j < AREA_SIZE; j++) {
             printf("%2c", walk_area[i][j]);
         }
-        printf("\n");
+        putchar('\n');
     }
 }
 
 
-int main(){
+int main() {
     char walk_area[AREA_SIZE][AREA_SIZE];
 
     printf("before walk\n");
