040cb56d4e5602989f44a7f973f47648a24a2a1f
Kit <4jrkit1@gmail.com>
2026-03-20 19:28:02 +0100

    completed selection sort

--- incomplete/selection_sort.c
+++ incomplete/selection_sort.c
@@ -5,18 +5,19 @@
 #define N 6
 
 void input(int arr[]);
-void sort(int arr[]);
+void sort(int arr[], int n);
 void print(int arr[]);
 
 int main(){
     int numbers[N];
+    int n = N;
 
     // get input and print arr
     input(numbers);
     print(numbers);
     
     // sort arr  and print
-    sort(numbers);
+    sort(numbers, n);
     print(numbers);
 
 
@@ -26,6 +27,39 @@ int main(){
 void input(int arr[]){
     printf("enter 6 numbers in the arr: ");
     for(int i = 0; i < N; i++){
-        scanf("% d", arr[i]);
+        scanf("%d,", &arr[i]);
     }
+}
+void print(int arr[]){
+    printf("[");
+    for(int i = 0; i < N; i++){
+        printf("%d, ", arr[i]);
+    }
+    printf("]\n");
+}
+void sort(int arr[], int n){
+    int max = 0;
+    int temp, index;
+
+    //  exit condition
+    if(n == 0){
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        if(arr[i] >= max){
+            max = arr[i];
+        }
+        // getting index of max element
+        if(max == arr[i]){
+            index = i;
+        }
+    }
+
+    // swapping 
+    temp = arr[n - 1];
+    arr[n - 1] = max;
+    arr[index] = temp;
+
+    // recall
+    sort(arr, n - 1);
 }
