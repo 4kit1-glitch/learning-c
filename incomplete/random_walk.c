8bdf9b2f15c70b0888d97a82b5aa0321e814fa4f
Kit <4jrkit1@gmail.com>
2026-03-20 19:49:11 +0100

    feat: added print_area function

--- incomplete/random_walk.c
+++ incomplete/random_walk.c
@@ -22,7 +22,14 @@ void generate_random_walk(char walk_area[][AREA_SIZE]){
     
 
 }
-void print_area(char arr[][AREA_SIZE]);
+void print_area(char walk_area[][AREA_SIZE]){
+    for (int i = 0; i < AREA_SIZE; i++){
+        for (int j = 0; j < AREA_SIZE; j++){
+            printf("%c", walk_area[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 
 int main(){
