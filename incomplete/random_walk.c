363e93b8bfdcad2d002afa7d572a569073aaff2a
Kit <4jrkit1@gmail.com>
2026-03-20 20:50:14 +0100

    fix: configure move behavior

--- incomplete/random_walk.c
+++ incomplete/random_walk.c
@@ -6,6 +6,7 @@
 #include<time.h>
 
 #define AREA_SIZE 10
+#define MOVE_COUNT 4
 
 #include<stdio.h>
 
@@ -15,12 +16,41 @@ void create_walk_area(char walk_area[][AREA_SIZE]){
             walk_area[i][j] = '.';
         }
     }
-
 }
 void generate_random_walk(char walk_area[][AREA_SIZE]){
+    int move, row, col, next_row, next_col;
+    char indicator = 'A'; // indicator what diplays after every move
+    char next_move;
 
+    srand((unsigned) time(NULL));
+
+    row = 0;
+    col = 0;
+
+    // start position
+    walk_area[row][col] = indicator;
+
+    next_move = rand() % MOVE_COUNT;
+
+    // move behavior
+    if(next_move == 0){
+        next_row = row - 1;
+        next_col = col;
+    }
+    else if(next_move == 1){
+        next_row = row + 1;
+        next_col = col;
+    }
+    else if(next_move == 2){
+        next_col = col - 1;
+        next_row = row;
+    }
+    else if(next_move == 3){
+        next_col = col + 1;
+        next_row = row;
+    }
     
-
+    
 }
 void print_area(char walk_area[][AREA_SIZE]){
     for (int i = 0; i < AREA_SIZE; i++){
