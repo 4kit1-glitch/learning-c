681bf8b4a9bb7ff0d1a1213ef612ce34d4dd331d
Kit <4jrkit1@gmail.com>
2026-03-20 19:45:32 +0100

    feat: created walking area

--- incomplete/random_walk.c
+++ incomplete/random_walk.c
@@ -10,7 +10,12 @@
 #include<stdio.h>
 
 void create_walk_area(char walk_area[][AREA_SIZE]){
-    
+    for(int i = 0; i < AREA_SIZE; i++){
+        for(int j = 0; j < AREA_SIZE; j++){
+            walk_area[i][j] = '.';
+        }
+    }
+
 }
 void generate_random_walk(char walk_area[][AREA_SIZE]){
 
