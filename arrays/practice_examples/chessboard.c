1e223068d8f4a82f8af098ef16f81d2c4e057a59
Kit <4jrkit1@gmail.com>
2026-04-06 18:57:32 +0100

    restructured code for chessboard.c - following recommended standards

--- arrays/practice_examples/chessboard.c
+++ arrays/practice_examples/chessboard.c
@@ -7,18 +7,15 @@
 #define PIECE_NUM  6
 
 int main(){
-
     char chess_board[BOARD_SIZE][BOARD_SIZE];
     char pieces_code[PIECE_NUM] = {'r','n','b','q','k','p'};
     char black = '.';
     char white = ' ';
 
-    for(int i = 0; i < BOARD_SIZE ; i++){
-
-        switch (i){
-
+    for (int i = 0; i < BOARD_SIZE ; i++) {
+        switch (i) {
             case 0:
-                for(int j = 0; j < BOARD_SIZE; j++){   
+                for (int j = 0; j < BOARD_SIZE; j++) {   
                     
                     chess_board[i][j] = pieces_code[j];
 
@@ -30,27 +27,24 @@ int main(){
                     }
                 }
             break;
-
             case 1:
-                for(int j = 0; j < BOARD_SIZE; j++){
+                for (int j = 0; j < BOARD_SIZE; j++) {
 
                     chess_board[i][j] = pieces_code[5];
                 }
             break;
-
             case 6:
-                for(int j = 0; j < BOARD_SIZE; j++){
+                for (int j = 0; j < BOARD_SIZE; j++) {
 
                     chess_board[i][j] = toupper(pieces_code[5]);
                 }
             break;
-
             case 7:
-                for(int j = 0; j < BOARD_SIZE; j++){   
+                for(int j = 0; j < BOARD_SIZE; j++) {   
                     
                     chess_board[i][j] = toupper(pieces_code[j]);
 
-                    if(j > 4){
+                    if (j > 4) {
                         chess_board[i][5] = toupper(pieces_code[2]);
                         chess_board[i][6] = toupper(pieces_code[1]);
                         chess_board[i][7] = toupper(pieces_code[0]);
@@ -58,43 +52,36 @@ int main(){
                     }
                 }
             break;
-
             case 3:
             case 5:
-                for(int j = 0; j < BOARD_SIZE; j++){
-                    if(j%2 == 0 ){
+                for (int j = 0; j < BOARD_SIZE; j++) {
+                    if (j%2 == 0 ) {
                         chess_board[i][j] = black;
                     }
-                    else{
+                    else {
                         chess_board[i][j] = white;
                     }
                 }
             break;
-
             case 2:
             case 4:
-                for(int j = 0; j < BOARD_SIZE; j++){
-                    if(j%2 == 0 ){
+                for (int j = 0; j < BOARD_SIZE; j++) {
+                    if (j%2 == 0 ) {
                         chess_board[i][j] = white;
                     }
-                    else{
+                    else {
                         chess_board[i][j] = black;
                     }
                 }
             break;
-        
-            default:
-                break;
-        
+            default: break;
         }
 
     }
 
-
     // printing the board
-
-    for(int i = 0;i < BOARD_SIZE; i++){
-        for(int j = 0; j< BOARD_SIZE; j++){
+    for (int i = 0;i < BOARD_SIZE; i++) {
+        for (int j = 0; j< BOARD_SIZE; j++) {
             printf("%3c",chess_board[i][j]);
         }
         printf("\n");
--- arrays/practice_examples/repnum.c
+++ arrays/practice_examples/repnum.c
@@ -1,49 +1,37 @@
 /*
-*   Date 26/02/26
+*  Date 26/02/26
 *  Program checks for repeating digits
 */
 
 #include<stdio.h>
 #include<stdbool.h>
+
 #define TEN 10
 #define ZERO 0
 
-int main()
-{
-    bool digit_seen[10] ={false};
+int main(){
+    bool digit_seen[10] = {false};
     long num;
     int digit;
     
     printf("Enter a number");
     scanf("%d",&num);
 
-    while (num > 0)
-    {
+    while (num > 0) {
         digit = num % TEN;
 
-        if(digit_seen[digit])
-        {
+        if (digit_seen[digit]) {
             break;
         }
         digit_seen[digit] = true;
         num /= TEN;
-
-    
     }
-    if(num > 0)
-    {
+    if (num > 0) {
         printf("contains repeating digits\n");
     }
-    else
-    {
+    else {
         printf("no repeating digit\n");
     }
     
-
-
-
-
-
-
     return 0;
 }
